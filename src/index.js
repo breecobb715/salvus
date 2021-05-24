8971bb43de237ec0ac427bc5b35925ebf0f8723c
Bree Cobb <66258021+breecobb715@users.noreply.github.com>
2021-05-24 09:45:03 -0400

    Updated complete page function, email function, and text function

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ window.showText = async function() {
     var given_id = document.getElementById("ID").value;
     let result;
     await axios.get(`http://localhost:3000/students/${given_id}`).then(function(response) {
-        result = response.data[0].first_name + " " + response.data[0].last_name
+        result = response.data[0].first_name + " " + response.data[0].last_name;
     });
     document.getElementById("name-confirm").innerHTML = "Your name is " + result
     if (text.style.visibility === "hidden") {
@@ -36,35 +36,54 @@ window.submitForm = async function() {
     }else{
         student_location = document.getElementById("location_input").value;
     }
+    let contact_method, contact_info
     await axios.get(`http://localhost:3000/students/${given_id}`).then(function(response) {
-        result = response.data[0].first_name + " " + response.data[0].last_name
+        contact_method = response.data[0].parent_contact_method;
+        contact_info = response.data[0].parent_contact_info;
     });
+    if(contact_method == "email"){
+        sendEmail();
+    }else if(contact_method == "text"){
+        sendText();
+    }else{
+        console.log("No contact method found!");
+    }
 }
 async function sendEmail() { 
-    let result
+    let name, status, location
     await axios.get(`http://localhost:3000/students/${given_id}`).then(function(response) {
-        result = response.data[0].first_name + " " + response.data[0].last_name
+        name = response.data[0].first_name + " " + response.data[0].last_name;
+        status = response.data[0].status;
+        location = response.data[0].location;
     });
     Email.send({ 
       Host: "smtp.gmail.com", 
       Username: "breecobb715@gmail.com", 
-      Password: "Enter your password", 
+      Password: process.env.EMAIL_PASS, 
       To: 'breecobb715@gmail.com', 
       From: "breecobb715@gmail.com", 
       Subject: "Salvus Student Status Update",
-      Body: `Your child ${result} has updated their status to STATUS at LOCATION. Please take necessary actions to secure your student's safety, and remind them to update their status to \"At Home\"`,
+      Body: `Your child ${result} has updated their status to ${status} at ${location}. Please take necessary actions to secure your student's safety, and remind them to update their status to \"At Home\"`,
     }) 
       .then(function (message) { 
-        alert("mail sent successfully") 
+        console.log("Email sent")
       }); 
 }
-client.messages
-    .create({
-        body: `Your child JOHN DOE has updated their status to STATUS at LOCATION. Please take necessary actions to secure your student's safety, and remind them to update their status to \"At Home\"`,
-        from: '+13252080653',
-        to: '+17203629336'
-    })
-    .then(message => console.log(message.sid));
+async function sendText(){
+    let name, status, location
+    await axios.get(`http://localhost:3000/students/${given_id}`).then(function(response) {
+        name = response.data[0].first_name + " " + response.data[0].last_name;
+        status = response.data[0].status;
+        location = response.data[0].location;
+    });
+    client.messages
+        .create({
+            body: `Your child ${name} has updated their status to ${status} at ${location}. Please take necessary actions to secure your student's safety, and remind them to update their status to \"At Home\"`,
+            from: '+13252080653',
+            to: '+17203629336'
+        })
+        .then(message => console.log(message.sid));
+}
 
 function updateLocation(){
     const successfulLookup = (position) => {
@@ -78,5 +97,19 @@ function updateLocation(){
         alert("Geolocation API is not supported in your browser")
     }
 }
-function unsubmitForm(){
+window.updateComplete = async function() {
+    let name, parent, status, location;
+    await axios.get(`http://localhost:3000/students/${given_id}`).then(function(response) {
+        name = response.data[0].first_name.toUpperCase() + " " + response.data[0].last_name.toUpperCase()
+        parent = response.data[0].parent_first_name.toUpperCase() + " " + response.data[0].last_name.toUpperCase()
+        status = response.data[0].status.toUpperCase()
+        location = response.data[0].location.toUpperCase()
+    });
+    document.getElementById("base-info").innerHTML = `${name}, your guardian ${parent} has been notified that you are ${status} at ${location}.`
+    document.getElementById("unsubmit-info").innerHTML = `If you are not ${name}, please press "Unsubmit Form" to undo your changes.`
+    if (text.style.visibility === "hidden") {
+        text.style.visibility = "visible";
+    } else {
+        text.style.visibility = "hidden";
+    }
 }
